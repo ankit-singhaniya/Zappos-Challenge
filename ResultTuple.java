9bb8996a4213a05e5ab6a22c8076f3adb8a1155b
ankit-singhaniya <ankit.singhaniya@gmail.com>
2014-10-03 18:04:52 -0700

    Create ResultTuple.java

---
+++ ResultTuple.java
@@ -0,0 +1,52 @@
+/* Created for an Entry Level Challlenge for Zappos Summer Internship 2015.
+ */
+package giftsearch;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ankit
+ */
+public class ResultTuple implements Comparable {
+
+    public ResultTuple(ArrayList<Product> products, float priceDifference) {
+        if (Float.isNaN(priceDifference)) {
+            priceDifference = Float.MAX_VALUE;
+        }
+        this.priceDifference = priceDifference;
+        this.products = products;
+    }
+    private float priceDifference = 0;
+
+    public float getPriceDifference() {
+        return priceDifference;
+    }
+
+    public void setPriceDifference(float priceDifference) {
+        this.priceDifference = priceDifference;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+    private ArrayList<Product> products = null;
+
+    @Override
+    public int compareTo(Object t) {
+        return Double.compare(this.priceDifference, ((ResultTuple) t).getPriceDifference());
+    }
+
+    @Override
+    public boolean equals(Object r) {
+        boolean eq = (((ResultTuple) r).getPriceDifference() == this.getPriceDifference());
+        if (eq) {
+            eq = (((((ResultTuple) r).getProducts()).containsAll(this.getProducts())) && (this.getProducts().containsAll(((ResultTuple) r).getProducts())));
+        }
+        return eq;
+    }
+}
